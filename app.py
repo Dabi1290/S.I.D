e50fe1db9b63a3c40fce94f6c3e494d083cf8e92
Dabi1290 <davide.amoruso33@gmail.com>
2023-12-10 14:55:52 +0100

    demo funzionante

--- app.py
+++ app.py
@@ -5,13 +5,44 @@ from sklearn.preprocessing import MinMaxScaler
 from scipy.sparse import hstack
 import streamlit as st
 
+models = {}
 
 scaler = MinMaxScaler()
+
 prompt = st.chat_input("Say something")
+with open('models/gnb.sav', 'rb') as file:
+    gnb = pickle.load(file)
+with open('models/bnb.sav', 'rb') as file:
+    bnb = pickle.load(file)
+with open('models/mnb.sav', 'rb') as file:
+    mnb = pickle.load(file)
 with open('models/svc.sav', 'rb') as file:
-    loaded_model = pickle.load(file)
+    svc = pickle.load(file)
+with open('models/lsvc.sav', 'rb') as file:
+    lsvc = pickle.load(file)
+with open('models/knc.sav', 'rb') as file:
+    knc = pickle.load(file)
+with open('models/rnc.sav', 'rb') as file:
+    rnc = pickle.load(file)
+with open('models/dtc.sav', 'rb') as file:
+    dtc = pickle.load(file)
+with open('models/etc.sav', 'rb') as file:
+    etc = pickle.load(file)
+with open('models/rfc.sav', 'rb') as file:
+    rfc = pickle.load(file)
+with open('models/retc.sav', 'rb') as file:
+    retc = pickle.load(file)
+
+
 with open('models/count_vectorizer.sav', 'rb') as file:
     vectorizer = pickle.load(file)
+
+
+headers = ['Gaussian Naive Bayes', 'Bernoulli Naive Bayes', 'Multinomial Naive Bayes',
+           'Support Vector Classifier', 'Linear Support Vector Classifier',
+           'K Nearest Classifier', 'Radius Nearest Classifier', 'Decision Tree Classifier',
+           'Extra Tree Classifier', 'Random Forest Classifier', 'Random Forest Extra Trees Classifier']
+values = []
 def remove_select_at_start(query):
     if query.startswith('select'):
         return query[len('select'):].lstrip()
@@ -33,10 +64,34 @@ def clean_data(input_string):
     return df.toarray()
 
 
-def predict(input):
-    input = clean_data(input)
-    return loaded_model.predict(input)
+def predict(input, modelo):
+    input2 = clean_data(input)
+    return modelo.predict(input2)
 
+def color_negative_red(value):
+    color=''
+    if value == 'Dannosa':
+        color = 'red'
+    elif value=='Normale':
+        color='green'
+    return f'background-color: {color}'
+def validate(result):
+    if result==1:
+        return "Dannosa"
+    else: return "Normale"
 if prompt:
-
-    st.write(predict(prompt))
+    values=[]
+    values.append(validate(predict(prompt,gnb)))
+    values.append(validate(predict(prompt, bnb)))
+    values.append(validate(predict(prompt, mnb)))
+    values.append(validate(predict(prompt, svc)))
+    values.append(validate(predict(prompt, lsvc)))
+    values.append(validate(predict(prompt, knc)))
+    values.append(validate(predict(prompt, rnc)))
+    values.append(validate(predict(prompt, dtc)))
+    values.append(validate(predict(prompt, etc)))
+    values.append(validate(predict(prompt, rfc)))
+    values.append(validate(predict(prompt, retc)))
+    df = pd.DataFrame({'Nomi modelli': headers, 'Valori predetti': values})
+    df= df.style.applymap(color_negative_red)
+    st.write(df)
